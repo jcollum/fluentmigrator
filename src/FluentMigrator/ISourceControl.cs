1fe47ba41e00d24eb8d9dc33057d1ba51265b53d
jcollum <jcollum@gmail.com>
2011-05-20 09:02:02 -0700

    forgot to add these on last commit
    Signed-off-by: jcollum <jcollum@gmail.com>

---
+++ src/FluentMigrator/ISourceControl.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace FluentMigrator
+{
+    public interface ISourceControl
+    {
+        string BasePath { get; set; }
+        string GetTextFile(string path, int revision);
+    }
+}
---
+++ src/FluentMigrator/SourceControl/Svn.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SharpSvn;
+using System.IO;
+
+namespace FluentMigrator.SourceControl
+{
+    public class Svn : ISourceControl
+    {
+        #region ISourceControl Members
+
+        public string BasePath { get; set; }
+
+        public string GetTextFile(string path, int revision)
+        {
+            using (SvnClient client = new SvnClient())
+            using (var stream = new MemoryStream())
+            {
+                // get the svn repo info
+                Uri repos = new Uri(this.BasePath);
+                client.Write(new SvnUriTarget(
+                        string.Format("{0}/{1}", this.BasePath, path), revision), stream);
+                stream.Position = 0;
+                // write the target to text 
+                StreamReader reader = new StreamReader(stream);
+                string text = reader.ReadToEnd();
+                stream.Position = 0;
+                return text;
+            }
+        }
+
+        #endregion
+    }
+}
